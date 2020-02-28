37b2d72faab37a3db45783f3d868a6dc1544c631
MaximMukhametov <59606832+MaximMukhametov@users.noreply.github.com>
2020-02-28 17:18:11 +0700

    set env vars

--- bot/views.py
+++ bot/views.py
@@ -1,14 +1,8 @@
-from django.shortcuts import render
-
-__author__ = '@begyy'
 from rest_framework.response import Response
-from rest_framework.views import  APIView
+from rest_framework.views import APIView
 from .models import Rates
-import telebot
-from django.conf import settings
+import telebot, os, requests
 from datetime import datetime, date, timedelta
-import requests
-from numpy import *
 from matplotlib.pyplot import *
 
 
@@ -18,8 +12,10 @@ res = requests.get(api_url)
 data = res.json()
 time_update = datetime.now()
 
-bot = telebot.TeleBot('927405192:AAETeWCBMhf9wLbgLSn_YRd1CqqyZn7ja9M')
+bot = telebot.TeleBot(os.environ.get('BOT_TOKEN'))
 
+
+# set webhook
 class UpdateBot(APIView):
     def post(self,request):
         json_string = request.body.decode("UTF-8")
@@ -29,14 +25,39 @@ class UpdateBot(APIView):
         return Response({'code': 200})
 
 
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.send_message(message.chat.id, 'Write a chat "/" to see a list of commands\n'
+                                   '/start - help text\n'
+                                   '/list or /lst - returns list of all available rates\n'
+                                   '/exchange $10 to CAD or  /exchange 10 USD to CAD - converts to \n'
+                                   'the second currency with two decimal precision and return.\n'
+                                   '/history USD/CAD for N days - return an image graph chart which\n'
+                                   'shows the exchange rate graph/chart of the selected currency for the last N days.')
+
+
+@bot.message_handler(commands=['list', 'lst'])
+def show_list(message):
+    textrates = ""
+    if time_update < datetime.now()-timedelta(minutes=10):
+        show_list_update_base(textrates, message)
+
+    # if less than 10 minutes have passed, we take data from the database.
+    else:
+        for base in Rates.objects.all():
+            textrates += ('%s: %s %s' % (base.name, base.value, '\n'))
+        bot.send_message(message.chat.id, textrates)
+
+
 # this function is used to update the database
 # if there were no queries in the last 10 minutes.
-def update_base(textrates, message):
+def show_list_update_base(textrates, message):
     global time_update
 
     # fix the time of the last database update
     time_update = datetime.now()
 
+    # base cleaning
     r = Rates.objects.all()
     r.delete()
 
@@ -47,29 +68,6 @@ def update_base(textrates, message):
         r.save()
     bot.send_message(message.chat.id,textrates)
 
-
-@bot.message_handler(commands=['start'])
-def start(message):
-    bot.send_message(message.chat.id,'Write a chat "/" to see a list of commands\n'
-                                   '/start - help text\n'
-                                   '/list or /lst - returns list of all available rates\n'
-                                   '/exchange $10 to CAD or  /exchange 10 USD to CAD - converts to \n'
-                                   'the second currency with two decimal precision and return.\n'
-                                   '/history USD/CAD for N days - return an image graph chart which\n'
-                                   'shows the exchange rate graph/chart of the selected currency for the last N days.')
-
-
-@bot.message_handler(commands=['list','lst'])
-def show_list(message):
-    textrates = ""
-    if time_update < datetime.now()-timedelta(minutes=10):
-        update_base(textrates, message)
-
-    # if less than 10 minutes have passed, we take data from the database.
-    else:
-        for base in Rates.objects.all():
-            textrates += ('%s: %s %s' % (base.name, base.value, '\n'))
-        bot.send_message(message.chat.id,textrates)
 
 @bot.message_handler(commands=['exchange'])
 def exchange(message):
@@ -84,9 +82,10 @@ def exchange(message):
             val1, val1_name = float(data['rates'][args[-3].upper()]), args[-3].upper()
             value = float(args[0])
         result = val2*value/val1
-        bot.send_message(message.chat.id,'%s %s = %.2f %s' % (value, val1_name, result, val2_name))
+        bot.send_message(message.chat.id, '%s %s = %.2f %s' % (value, val1_name, result, val2_name))
     except:
-        bot.send_message(message.chat.id,'Invalid input format.\nexample of the correct format:\n/exchange 10 DKK to CZK, or $10 to RUB')
+        bot.send_message(message.chat.id, 'Invalid input format.\nexample of the correct format:\n/exchange 10 DKK to CZK, or $10 to RUB')
+
 
 @bot.message_handler(commands=['history'])
 def history(message):
@@ -122,6 +121,6 @@ def history(message):
         title('Exchange rate for the last %s days' % days)
         savefig('myfig.png')
         graph = open('myfig.png', 'rb')
-        bot.send_photo(message.chat.id,graph)
+        bot.send_photo(message.chat.id, graph)
     except:
         bot.send_message(message.chat.id, 'No exchange rate data is available for the selected currency.')
